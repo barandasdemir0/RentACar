926debcb8a4810cacf1224537e8e48110f533b85
Baran Daşdemir <76569348+barandasdemir0@users.noreply.github.com>
2026-06-12 23:28:33 +0300

    Domain katmanına testler uygulandı

---
+++ RentACar.DomainTest/CarTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using RentACar.Domain.Entities.Vehicles.Cars;
+using RentACar.Domain.Entities.Vehicles.Cars.Enum;
+using RentACar.Domain.Entities.Vehicles.Cars.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace RentACar.DomainTest;
+
+public class CarTests
+{
+
+    // Yeni araba yaratıldığında kurallar doğru işliyor mu
+    [Fact]
+    public void CreateCar_WithValidParameters_ShouldInitializeCorrectly()
+    {
+        //arrange
+        var brandId = Guid.CreateVersion7();
+        var modelWithSpaces = "   M3 Sedan   ";
+        var images = new CarImages("https://link.com/cover.jpg", "https://link.com/big.jpg");
+        var specs = new CarSpecifications(1000, "Otomatik", 5, 2, "Benzin");
+        //act
+        var car = new Car(brandId, modelWithSpaces, images, specs);
+        //assert
+        car.BrandId.Should().Be(brandId);
+        car.Model.Should().Be("M3 Sedan");
+        car.Status.Should().Be(CarStatus.Available);
+        car.Features.Should().BeEmpty();
+    }
+
+    //Müsait bir araç başarıyla kiralanabiliyormu
+    [Fact]
+    public void RentCar_WhenCarIsAvailable_ShouldChangeStatusToRented()
+    {
+        //arrange
+        var car = new Car(Guid.CreateVersion7(), "M3",
+            new CarImages("https://link.com/cover.jpg", "https://link.com/big.jpg"),
+            new CarSpecifications(1000, "Otomatik", 5, 2, "Benzin")
+            );
+
+        //act
+        car.RentCar();
+        //assert
+        car.Status.Should().Be(CarStatus.Rented);
+    }
+
+    //bakımdaki araç kiralanabiliyormu testi
+    [Fact]
+    public void RentCar_WhenCarIsNotAvailable_ShouldThrowInvalidOperationException()
+    {
+        //arrange
+         var car = new Car(Guid.CreateVersion7(), "M3",
+            new CarImages("https://link.com/cover.jpg", "https://link.com/big.jpg"),
+            new CarSpecifications(1000, "Otomatik", 5, 2, "Benzin"));
+
+        car.SendToMaintenance();
+
+        //act
+        Action act = () => car.RentCar();
+
+        //assert
+        act.Should().Throw<InvalidOperationException>().WithMessage("Müsait olmayan araç kiralanamaz");
+    }
+}
